5070f38865ed51e582c93e73ea673a6e672ce8f0
M Nunez <futuristacid@proton.me>
2025-06-15 15:49:32 -0600

    Basic Animation

--- 25_06.cljs
+++ 25_06.cljs
@@ -36,21 +36,22 @@
   (do (set! (.-width canvas) w)
       (set! (.-height canvas) h)))
 
-(do
-  (resize-canvas 0 0)
-  (scale 1 1))
-(do
-  (let [colors []
-        xs (range 8)
-        ys (range 8)]
-    (doseq [x xs y ys]
-      (when (< (rand) 0.2)
-        (fill-style (str "rgb("
-                         (rand-int 255) " "
-                         (rand-int 255) " "
-                         (rand-int 255) " / "
-                         (rand 0.5) ")"))
-        (fill-rect x y 1 1)))))
+(comment
+  (do
+    (resize-canvas (* 3 5 8) (* 5 8))
+    (scale 5 5))
+  (do
+    (let [colors []
+          xs (range 8)
+          ys (range 8)]
+      (doseq [x xs y ys]
+        (when (< (rand) 0.2)
+          (fill-style (str "rgb("
+                           (rand-int 255) " "
+                           (rand-int 255) " "
+                           (rand-int 255) " / "
+                           (rand 0.5) ")"))
+          (fill-rect x y 1 1))))))
 
 (range 10)
 (range 10 20)
@@ -86,3 +87,76 @@
 (split-at 5 (range 10))
 (split-with #(<= % 10) (range 0 20 2))
 (take 10 (cycle (range 3)))
+
+
+(defn rand-px []
+  {:x (rand-int 8)
+   :y (rand-int 8)
+   :r (rand-int 255)
+   :g (rand-int 255)
+   :b (rand-int 255)})
+
+(def pixels (take 42 (repeatedly rand-px)))
+
+(defn sprite-sheet-alpha [pxls n]
+  (resize-canvas (* 8 n) 8)
+  (doseq [i (range n)]
+    (doseq [p pxls]
+      (fill-style (str "rgb("
+                       (:r p) " "
+                       (:g p) " "
+                       (:b p) " / "
+                       (/ 1 (inc i)) ")"))
+      (fill-rect (:x p) (:y p) 1 1))
+    (translate 8 0)))
+
+(comment
+  (sprite-sheet-alpha pixels 5)
+  )
+
+(defn draw-image [img sx sy sw sh dx dy dw dh]
+  (.drawImage ctx img sx sy sw sh dx dy dw dh))
+
+(def img (js/Image.))
+
+(defn animate []
+  (resize-canvas 8 8)
+  (draw-image img 0 0 8 8 0 0 8 8))
+
+(comment
+  (.addEventListener img "load" #(animate))
+  (set! (.-src img) "img.png")
+  )
+
+(defn points [w h]
+  (iterate
+   (fn [v]
+     (let [n (rand-int 2)]
+       (assoc v n
+              ((if (pos? (rand-int 2)) inc dec)
+               (nth v n)))))
+   [(rand-int w) (rand-int h)]))
+
+(def ps (set (take 1000000 (points 256 256))))
+
+(def moves [[-1 -1] [-1 0] [-1 1]
+            [0 -1] [0 1]
+            [1 -1] [1 0] [1 1]])
+
+(comment
+  (do (resize-canvas 512 512)
+      (scale 1 1)
+      (fill-style "FireBrick"))
+
+  (animate)
+  )
+
+(defn inc-or-dec []
+  (if (pos? (rand-int 2)) inc dec))
+
+(defn animate []
+  (clear-rect -256 -256 1024 1024)
+  (apply translate (rand-nth moves))
+  (doseq [p ps] (fill-rect (first p) (second p) 1 1))
+  (.requestAnimationFrame js/window #(animate)))
+
